e6af3a12b88b36acdc5393bc4ebcf0264cf1e680
Patrick Alexis <alexispatrick77@gmail.com>
2022-07-27 13:55:09 -0700

    Initialize project with docker for postgres db

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val postgresql_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.7.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+val entryPoint = "com.patrickpie12345.ApplicationKt"
+
+group = "com.patrickpie12345"
+version = "0.0.1"
+application {
+    mainClass.set("com.patrickpie12345.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    // Postgres DB
+    implementation("org.postgresql:postgresql:$postgresql_version")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/com/patrickpie12345/Application.kt
@@ -0,0 +1,13 @@
+package com.patrickpie12345
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.patrickpie12345.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSerialization()
+        configureMonitoring()
+        configureRouting()
+    }.start(wait = true)
+}
