e77ef44cd7c21c96c6ce8ccb6d741445da0a350b
Patrick Alexis <alexispatrick77@gmail.com>
2022-08-08 23:05:06 -0700

    Add UUID type for graphql

--- src/main/kotlin/com/patrickpie12345/graphql/scalar/CustomSchemaGeneratorHooks.kt
+++ src/main/kotlin/com/patrickpie12345/graphql/scalar/CustomSchemaGeneratorHooks.kt
@@ -4,6 +4,7 @@ import com.expediagroup.graphql.generator.hooks.SchemaGeneratorHooks
 import graphql.schema.GraphQLScalarType
 import graphql.schema.GraphQLType
 import java.time.Instant
+import java.util.*
 import kotlin.reflect.KType
 
 class CustomSchemaGeneratorHooks : SchemaGeneratorHooks {
@@ -14,8 +15,15 @@ class CustomSchemaGeneratorHooks : SchemaGeneratorHooks {
         .coercing(InstantScalar)
         .build()
 
+    private val graphqlUUIDType: GraphQLScalarType = GraphQLScalarType.newScalar()
+        .name("UUID")
+        .description("A type representing a formatted java.util.UUID")
+        .coercing(UUIDScalar)
+        .build()
+
     override fun willGenerateGraphQLType(type: KType): GraphQLType? = when (type.classifier) {
         Instant::class -> graphqlInstantType
+        UUID::class -> graphqlUUIDType
         else -> super.willGenerateGraphQLType(type)
     }
 }
