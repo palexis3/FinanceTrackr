6ae45c4be1d7718412d3d84d74bc163998262660
Patrick Alexis <alexispatrick77@gmail.com>
2023-01-17 13:58:52 -0800

    Added stacktrace logs

--- src/main/kotlin/com/patrickpie12345/server/api/routing/ImageRoutes.kt
+++ src/main/kotlin/com/patrickpie12345/server/api/routing/ImageRoutes.kt
@@ -53,7 +53,10 @@ fun Route.imageRouting(
                 }
             } ?: call.respondText("Internal error: image processed was null", status = HttpStatusCode.InternalServerError)
         } catch (ex: Exception) {
-            call.application.environment.log.debug("ImageRoutes exception: ${ex.stackTrace}")
+            call.application.environment.log.debug("ImageRoutes exception: $ex")
+            for(exceptionElement in ex.stackTrace) {
+                call.application.environment.log.debug("$exceptionElement")
+            }
             call.respondText(
                 "Exception occurred while processing image",
                 status = HttpStatusCode.InternalServerError
